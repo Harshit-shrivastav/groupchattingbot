cd12608457fcf94572109faec942362040e7fd71
pyrogrammer-s <92732265+pyrogrammer-s@users.noreply.github.com>
2021-11-14 11:55:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 CHAT_ID = int(os.environ.get("CHAT_ID", 0))
 USER_ID = int(os.environ.get("USER_ID", 0))
 Bot = Client(
-    "Glitch-Art-Bot",
+    "Group-chatting-bot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
